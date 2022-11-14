e8d53988b2aa91451d472906f5f70fc889243cfa
yufunz <yufu@example.com>
2022-11-14 18:35:50 +1300

    add color for toolbar menus

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,54 +1,72 @@
 <template>
   <div id="toolbar">
-    <i @click="addOne" class="glyphicon glyphicon-plus"></i>
-    <i @click="toggleFavorite" class="glyphicon glyphicon-star" v-bind:class="{starred:activeNote.favorite}"></i>
-    <i @click="deleteNote" class="glyphicon glyphicon-remove"></i>
+    <i id="add" @click="addOne" class="glyphicon glyphicon-plus"></i>
+    <i
+      id="star"
+      @click="toggleFavorite"
+      class="glyphicon glyphicon-star"
+      v-bind:class="{ starred: activeNote.favorite }"
+    ></i>
+    <i id="remove" @click="deleteNote" class="glyphicon glyphicon-remove"></i>
   </div>
 </template>
 
 <script>
 export default {
-  computed:{
-    activeNote(){
+  computed: {
+    activeNote() {
       return this.$store.getters.activeNote
     }
   },
-  methods:{
-    addOne(){
+  methods: {
+    addOne() {
       //AddNote distributed to actions via dispatch
-      this.$store.dispatch('addNote')
+      this.$store.dispatch("addNote")
     },
-    toggleFavorite(){
-      this.$store.dispatch('toggleFavorite')
+    toggleFavorite() {
+      this.$store.dispatch("toggleFavorite")
     },
-    deleteNote(){
-      this.$store.dispatch('deleteNote')
+    deleteNote() {
+      this.$store.dispatch("deleteNote")
     }
   }
 }
 </script>
 <style type="text/css">
-	
 #toolbar {
   float: left;
   width: 80px;
   height: 100%;
-  background-color: #30414D;
+  background-color: #30414d;
   color: #767676;
   padding: 35px 25px 25px 25px;
 }
+
 #toolbar i {
   font-size: 30px;
   margin-bottom: 35px;
   cursor: pointer;
-  opacity: 0.8;
+  opacity: 0.4;
   transition: opacity 0.5s ease;
+}
+
+#add {
+  color: greenyellow;
+}
+
+#star {
+  color: yellow;
+}
+
+#remove {
+  color: red;
 }
 
 #toolbar i:hover {
   opacity: 1;
 }
+
 .starred {
-  color: #F7AE4F;
+  color: #f7ae4f;
 }
 </style>
