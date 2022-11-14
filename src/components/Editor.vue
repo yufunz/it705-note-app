8c65c728093a53a7c25f180a2e6c234602ed3fc6
yufunz <yufu@example.com>
2022-11-14 19:46:32 +1300

    fix edit note title

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -19,6 +19,17 @@
 </template>
 
 <script>
+!(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0]
+  if (!d.getElementById(id)) {
+    js = d.createElement(s)
+    js.id = id
+    js.src = "https://weatherwidget.io/js/widget.min.js"
+    fjs.parentNode.insertBefore(js, fjs)
+  }
+})(document, "script", "weatherwidget-io-js")
+
 export default {
   computed: {
     activeNoteText() {
@@ -31,19 +42,6 @@ export default {
     }
   }
 }
-</script>
-
-<script>
-!(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0]
-  if (!d.getElementById(id)) {
-    js = d.createElement(s)
-    js.id = id
-    js.src = "https://weatherwidget.io/js/widget.min.js"
-    fjs.parentNode.insertBefore(js, fjs)
-  }
-})(document, "script", "weatherwidget-io-js")
 </script>
 
 <style type="text/css">
