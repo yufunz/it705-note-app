e48897a8c4d6f51fad51be5d82bdb10a97a0d100
yufunz <yufu@example.com>
2022-11-17 15:59:39 +1300

    add Widget component

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -5,31 +5,10 @@
       v-on:input="editNote"
       class="form-control"
     ></textarea>
-
-    <a
-      id="weather"
-      class="weatherwidget-io"
-      href="https://forecast7.com/en/n46d41168d35/invercargill/"
-      data-label_1="INVERCARGILL"
-      data-label_2="WEATHER"
-      data-theme="original"
-      >INVERCARGILL WEATHER</a
-    >
   </div>
 </template>
 
 <script>
-!(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0]
-  if (!d.getElementById(id)) {
-    js = d.createElement(s)
-    js.id = id
-    js.src = "https://weatherwidget.io/js/widget.min.js"
-    fjs.parentNode.insertBefore(js, fjs)
-  }
-})(document, "script", "weatherwidget-io-js")
-
 export default {
   computed: {
     activeNoteText() {
@@ -44,16 +23,4 @@ export default {
 }
 </script>
 
-<style type="text/css">
-#note-editor {
-  height: 100%;
-  margin-left: 380px;
-}
-
-#note-editor textarea {
-  resize: none;
-  height: 90%;
-  border: 0;
-  border-radius: 0;
-}
-</style>
+<style type="text/css"></style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import store from '../vuex/store'
+import Vue from "vue"
+import App from "./App"
+import store from "../vuex/store"
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   store,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
 })
